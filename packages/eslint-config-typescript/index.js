da7ef69cf49a58a9225feb300e2f4904745b74a4
W4rd3s <thomas.blavot@gmail.com>
2023-06-22 16:24:04 +0200

    feat(typescript): add packages for typescript support

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -4,9 +4,12 @@
 module.exports = {
   extends: [
   ].map(require.resolve),
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
+  root: true,
   rules: {},
 };
